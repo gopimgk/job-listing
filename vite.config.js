7387db14a4b1a42ad7b53cb2e11afbe5088726d1
gopimgk <gopimgk510@gmail.com>
2025-06-29 22:50:52 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-        },
-    },
+    ]
 });
